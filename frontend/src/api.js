85053d1af5a52ed03504deec5fe7e8d8b514411b
Hiteshi1601 <Hiteshimudham16@gmail.com>
2026-07-02 14:36:03 +0530

    first commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://blog-platform-j43c.onrender.com/api",
+  baseURL: window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
+    ? "http://localhost:5000/api"
+    : "https://blog-platform-j43c.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
